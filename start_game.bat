c23f237d9cc8bdc9ca01e8e49ce1ac2c15538eeb
Reythrix <Reythrix@github.com>
2025-10-18 18:13:41 -0700

    first changes

--- start_game.bat
+++ start_game.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting Super Mario Game...
 echo.
-echo The game will be available at: http://localhost:8000
+echo The game will be available at: http://localhost:8001
 echo.
 echo Press Ctrl+C to stop the server
 echo.
